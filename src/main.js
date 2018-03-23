6add26b1a8937abf7512e598ad468b7591db0bf7
Anuradha Dunuhappawa <joedunu@gmail.com>
2018-03-23 23:42:53 +1100

    Create routes

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
 
-Vue.config.productionTip = false
+import Home from './components/pages/home/Home.vue'
+import AboutUs from './components/pages/aboutUs/AboutUs.vue'
+import ContactUs from './components/pages/contactUs/ContactUs.vue'
+
+Vue.config.productionTip = false;
+
+const routes = [
+    { path: '/home', component: Home },
+    { path: '/about-us', component: AboutUs },
+    { path: '/contact-us', component: ContactUs }
+];
+
+const router = new VueRouter({
+    routes
+});
 
 new Vue({
-  render: h => h(App)
-}).$mount('#app')
+  render: h => h(App),
+    router
+}).$mount('#app');
