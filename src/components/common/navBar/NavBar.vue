02399401dba40404f9cf95606e162b5aac7d3fd8
Anuradha Dunuhappawa <joedunu@gmail.com>
2018-03-23 23:40:35 +1100

    Add nav-bar

---
+++ src/components/common/navBar/NavBar.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class='nav-bar'>
+        <nav-item label='Home' name='home' />
+        <nav-item label='About Us' name='about-us' />
+        <nav-item label='Contact Us' name='contact-us' />
+    </div>
+</template>
+
+<script>
+    import NavItem from '../navItem/NavItem.vue'
+
+    export default {
+        name: "nav-bar",
+        components: {
+            NavItem
+        }
+    }
+</script>
+
+<style scoped>
+    .nav-bar {
+        top: 0;
+        padding: 10px;
+        position: sticky;
+        background-color: dodgerblue;
+        display: flex;
+        z-index: 10;
+    }
+</style>
---
+++ src/components/common/navItem/NavItem.vue
@@ -0,0 +1,40 @@
+<template>
+    <router-link class='nav-item' v-bind:to='name' v-bind:name='name' v-on:click='goToPage'>
+        {{label}}
+    </router-link>
+</template>
+
+<script>
+    export default {
+        id: 'nav-item',
+        name: 'nav-item',
+        props: {
+            label: String,
+            name: String
+        },
+        methods: {
+            goToPage: function goToPage (e) {
+                console.log('clicked', e.target.name)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .nav-item {
+        background-color: dodgerblue;
+        display: flex;
+        padding: 10px;
+    }
+
+    button {
+        color: white;
+        border: 0;
+        font-size: 16px;
+        &:active,
+        &:focus {
+            text-decoration: underline;
+            border: none;
+        }
+    }
+</style>
